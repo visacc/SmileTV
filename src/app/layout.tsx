b0b64c8cf7d334850c3fc114ca803871f17773fd
vava <58034771+vavacc@users.noreply.github.com>
2025-08-01 09:39:53 +0800

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
   return {
     title: siteName,
-    description: '影视聚合',
+    description: '影视最佳线路',
     manifest: '/manifest.json',
   };
 }
